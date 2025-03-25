b434f95670db0363d458a23caaecf13ca3d4b01d
varangian-core <mikhaltsevichv@gmail.com>
2025-03-25 11:31:30 -0700

    Refactor: Simplify database setup to use Prisma migrations and proper credentials.

--- installation/initdb/01-setup.sql
+++ installation/initdb/01-setup.sql
@@ -1,10 +1,28 @@
+-- First connect to template1 to ensure we have a working connection
+\c template1
+
 DO $$
 BEGIN
+    -- Create user if not exists
     IF NOT EXISTS (SELECT FROM pg_catalog.pg_user WHERE usename = 'mindplace') THEN
         CREATE USER mindplace WITH PASSWORD 'mindplace';
     END IF;
+    
+    -- Create database if not exists
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mindplace') THEN
+        CREATE DATABASE mindplace WITH OWNER mindplace;
+    END IF;
 END $$;
 
-CREATE DATABASE mindplace WITH OWNER mindplace;
+-- Connect to the new database to set additional privileges
+\c mindplace
 
-GRANT ALL PRIVILEGES ON DATABASE mindplace TO mindplace;
+DO $$
+BEGIN
+    -- Ensure privileges
+    GRANT ALL PRIVILEGES ON DATABASE mindplace TO mindplace;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO mindplace;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO mindplace;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO mindplace;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TYPES TO mindplace;
+END $$;
